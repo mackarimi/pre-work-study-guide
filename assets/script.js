77d9421c7adf5c2ae6679b06a4ed797926943891
mackarimi <kkarimi47@gmail.com>
2023-05-11 12:22:57 -0700

    modified html, script

---
+++ assets/script.js
@@ -0,0 +1,20 @@
+const topic = "HTML";
+
+if (topic === "HTML") {
+  console.log("Let's study HTML!");
+} else if (topic === "CSS") {
+  console.log("Let's study CSS!");
+} else if (topic === "Git") {
+  console.log("Let's study Git!");
+} else if (topic === "JavaScript") {
+  console.log("Let's study JavaScript!");
+} else {
+  console.log("Please try again!");
+}
+
+const shapes = ["square", "rectangle", "circle", "triangle"];
+
+console.log(shapes[0]); // square
+console.log(shapes[1]); // rectangle
+console.log(shapes[2]); // circle
+console.log(shapes[3]); // triangle
