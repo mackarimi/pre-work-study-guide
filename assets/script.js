b180a80bef34a87478262b7f36ff700945b8910f
mackarimi <kkarimi47@gmail.com>
2023-05-11 12:44:04 -0700

    add for lop

--- assets/script.js
+++ assets/script.js
@@ -1,20 +1,28 @@
-const topic = "HTML";
+const shapes = ["square", "rectangle", "circle", "triangle"];
+for (let x = 0; x < shapes.length; x++) {
+// code block to be executed
+console.log(shapes[x]);
 
-if (topic === "HTML") {
-  console.log("Let's study HTML!");
-} else if (topic === "CSS") {
-  console.log("Let's study CSS!");
-} else if (topic === "Git") {
-  console.log("Let's study Git!");
-} else if (topic === "JavaScript") {
-  console.log("Let's study JavaScript!");
-} else {
-  console.log("Please try again!");
+// const topic = "HTML";
+
+// if (topic === "HTML") {
+//   console.log("Let's study HTML!");
+// } else if (topic === "CSS") {
+//   console.log("Let's study CSS!");
+// } else if (topic === "Git") {
+//   console.log("Let's study Git!");
+// } else if (topic === "JavaScript") {
+//   console.log("Let's study JavaScript!");
+// } else {
+//   console.log("Please try again!");
+// }
+
+// const shapes = ["square", "rectangle", "circle", "triangle"];
+
+// console.log(shapes[0]); // square
+// console.log(shapes[1]); // rectangle
+// console.log(shapes[2]); // circle
+// console.log(shapes[3]); // triangle
+
 }
 
-const shapes = ["square", "rectangle", "circle", "triangle"];
-
-console.log(shapes[0]); // square
-console.log(shapes[1]); // rectangle
-console.log(shapes[2]); // circle
-console.log(shapes[3]); // triangle
